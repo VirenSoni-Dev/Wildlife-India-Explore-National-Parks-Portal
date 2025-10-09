860a33043270ec2c040dbc3129ccfa5982fd938f
Viren Soni <71062471+VirenSoni-Dev@users.noreply.github.com>
2025-10-09 18:31:32 +0530

    Initial commit of the project

---
+++ js/nav.js
@@ -0,0 +1,27 @@
+const header = document.querySelector('header');
+const sidebarButton = document.getElementById('sidebar-toggle-button');
+const sidebarIcon = document.getElementById('sidebarIcon');
+
+function openSidebar() {
+   header.classList.add('sidebar-open');
+   sidebarButton.setAttribute('aria-expanded', 'true');
+   sidebarButton.innerHTML = '<i id="sidebarIcon" data-lucide="x"></i>';
+   lucide.createIcons();
+}
+
+function closeSidebar() {
+   header.classList.remove('sidebar-open');
+   sidebarButton.setAttribute('aria-expanded', 'false');
+   sidebarButton.innerHTML = '<i id="sidebarIcon" data-lucide="menu"></i>';
+   lucide.createIcons();
+}
+
+function toggleSidebar() {
+   const isMenuOpen = header.classList.contains('sidebar-open');
+
+   if (isMenuOpen) {
+      closeSidebar();
+   } else {
+      openSidebar();
+   }
+}
